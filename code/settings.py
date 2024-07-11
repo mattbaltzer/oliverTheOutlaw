0c1047d450d55e6f2179c5c6d557873bfea8b820
Go Away <baltzer.matt@gmail.com>
2024-07-11 16:38:18 -0700

    Created code folder and the empty main file, and the settings file

---
+++ code/settings.py
@@ -0,0 +1,18 @@
+import pygame, sys
+from pygame.math import Vector2 as vector
+
+window_width, window_height = 1280, 720
+tile_size = 64
+animation_speed = 6 
+
+# layers
+z_layers = {
+    'bg': 0,
+    'clouds': 1,
+    'bg tiles': 2, 
+    'path': 3,
+    'bg details': 4,
+    'main': 5,
+    'water': 6,
+    'fg': 7
+}
