10380736c157251c9f299d79c12ce747a6541e37
Go Away <baltzer.matt@gmail.com>
2024-07-16 12:39:54 -0700

    Created a camera that follows the player around instead of viewing a static part of the level

--- code/level.py
+++ code/level.py
@@ -1,13 +1,14 @@
 from settings import *
 from sprites import Sprite, MovingSprite
 from player import Player
+from groups import AllSprites
 
 class Level:
     def __init__(self, tmx_map):
         self.display_surface = pygame.display.get_surface()
 
         # groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
         self.semicollidable_sprites = pygame.sprite.Group()
 
@@ -21,7 +22,7 @@ class Level:
         # objects
         for obj in tmx_map.get_layer_by_name('Objects'):
             if obj.name == 'player':
-                Player((obj.x,obj.y),self.all_sprites, self.collision_sprites, self.semicollidable_sprites)
+                self.player = Player((obj.x,obj.y),self.all_sprites, self.collision_sprites, self.semicollidable_sprites)
 
         # moving objects
         for obj in tmx_map.get_layer_by_name('Moving Objects'):
@@ -37,8 +38,7 @@ class Level:
                 speed = obj.properties['speed']
                 MovingSprite((self.all_sprites, self.semicollidable_sprites), start_pos, end_pos, move_dir, speed)
                 
-
     def run(self, dt):
         self.display_surface.fill('black')
         self.all_sprites.update(dt)
-        self.all_sprites.draw(self.display_surface)
+        self.all_sprites.draw(self.player.hitbox_rect.center)
